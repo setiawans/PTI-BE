f0f7619f66962a3414d799c6b32d5663234ce6bd
Steven Setiawan <135805488+setiawans@users.noreply.github.com>
2024-05-26 16:27:20 +0700

    Update settings.py

--- oprec_pti/settings.py
+++ oprec_pti/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jlue0h&k^@27l=&8eu73wpvsqill3@0m*vfo$w3m+w1ze+kn90'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
